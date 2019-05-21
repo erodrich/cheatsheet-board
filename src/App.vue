f8b7db75a579cf70578c1f168ff59fbd5532f385
Eric Rodrich <acron.bn@gmail.com>
2019-05-20 22:15:44 -0500

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <md-card>
+      <md-card-header>
+        <div class="md-title">Material Design with Vue.js 2 Demo</div>
+        <div class="md-subhead">by CodingTheSmartWay.com</div>
+      </md-card-header>
+      <md-card-content>
+
+        <md-button class="md-raised md-primary" v-on:click="fillTable()">Fill table</md-button>
+        <md-button class="md-raised md-primary" v-on:click="clearTable()">Clear table</md-button>
+        <br />
+        <md-table md-card>
+            <md-table-row>
+              <md-table-head>First name</md-table-head>
+              <md-table-head>Last name</md-table-head>
+              <md-table-head>Email</md-table-head>
+            </md-table-row>
+            <md-table-row v-for="contact in contacts">
+              <md-table-cell>{{contact.firstname}}</md-table-cell>
+              <md-table-cell>{{contact.lastname}}</md-table-cell>
+              <md-table-cell>{{contact.email}}</md-table-cell>
+            </md-table-row>
+        </md-table>
+      </md-card-content>
+    </md-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      contacts: []
+    }
+  },
+  methods: {
+    fillTable: function () {
+      this.contacts.push({firstname: 'Sebastian', lastname: 'Eschweiler', email: 's.eschweiler@mail.com'})
+      this.contacts.push({firstname: 'Bill', lastname: 'Smith', email: 'b.smith@mail.com'})
+      this.contacts.push({firstname: 'Ann', lastname: 'Parker', email: 'a.parker@mail.com'})
+    },
+    clearTable: function () {
+      this.contacts.splice(0, this.contacts.length)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <md-card>
+      <md-card-header>
+        <div class="md-title">Material Design with Vue.js 2 Demo</div>
+        <div class="md-subhead">by CodingTheSmartWay.com</div>
+      </md-card-header>
+      <md-card-content>
+
+        <md-button class="md-raised md-primary" v-on:click="fillTable()">Fill table</md-button>
+        <md-button class="md-raised md-primary" v-on:click="clearTable()">Clear table</md-button>
+        <br />
+        <md-table>
+          <md-table-header>
+            <md-table-row>
+              <md-table-head>First name</md-table-head>
+              <md-table-head>Last name</md-table-head>
+              <md-table-head>Email</md-table-head>
+            </md-table-row>
+          </md-table-header>
+          <md-table-body>
+            <md-table-row v-for="contact in contacts">
+              <md-table-cell>{{contact.firstname}}</md-table-cell>
+              <md-table-cell>{{contact.lastname}}</md-table-cell>
+              <md-table-cell>{{contact.email}}</md-table-cell>
+            </md-table-row>
+          </md-table-body>
+        </md-table>
+      </md-card-content>
+    </md-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
